049ef52b838b7d0a3075787cf7d60fc184892073
Kyle Liao <99450559+thekyleliao@users.noreply.github.com>
2022-12-09 19:19:36 -0500

    Create quiz2.py

---
+++ quiz2.py
@@ -0,0 +1,20 @@
+score = 0
+def main():
+  do_quiz("What is 5+5 ", "10")
+
+def do_quiz(question, answer):
+  # We have attempts from 3 to 1
+  # range(start, stop, interval)
+  global score
+  for attempt in range(3, 0, -1):
+    inp = input(question)
+    if inp == answer:
+      score = score + 1
+      print("Correct Answer. " + "Your score is " + str(score) + ".")
+      return
+    else:
+      print("Try again, attempts remaining: ", attempt - 1)
+  print("You couldn't figure it out")
+
+
+main()
